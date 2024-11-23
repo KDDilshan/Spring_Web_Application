6b5f227c0203208d3c8ffdcabf1ccba4772d9528
KDDilshan <kdprojects847@gmail.com>
2024-11-23 08:15:38 +0530

    added mapping profile for Authors and add modelmapper configeuation

--- src/main/java/com/kavindu/testing/mappers/Mapper.java
+++ src/main/java/com/kavindu/testing/mappers/Mapper.java
@@ -1,4 +1,9 @@
 package com.kavindu.testing.mappers;
 
-public interface Mapper {
+import com.kavindu.testing.domain.dto.AuthorDto;
+import com.kavindu.testing.domain.entities.AuthorEntity;
+
+public interface Mapper<A,B> {
+    B mapTo(A a);
+    A mapFrom(B b);
 }
---
+++ src/main/java/com/kavindu/testing/mappers/impl/AuthorMapperImpl.java
@@ -0,0 +1,25 @@
+package com.kavindu.testing.mappers.impl;
+
+import com.kavindu.testing.domain.dto.AuthorDto;
+import com.kavindu.testing.domain.entities.AuthorEntity;
+import com.kavindu.testing.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class AuthorMapperImpl implements Mapper<AuthorEntity, AuthorDto> {
+
+    private ModelMapper modelMapper;
+
+    public AuthorMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public AuthorDto mapTo(AuthorEntity authorEntity) {
+        return  modelMapper.map(authorEntity, AuthorDto.class);
+    }
+
+    @Override
+    public AuthorEntity mapFrom(AuthorDto authorDto) {
+        return modelMapper.map(authorDto, AuthorEntity.class);
+    }
+}
