394d7212cc7f29ce0722d748afed83ca45f6489b
KDDilshan <kdprojects847@gmail.com>
2024-11-23 19:42:29 +0530

    added book reposiory

---
+++ src/main/java/com/kavindu/testing/respositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.kavindu.testing.respositories;
+
+import com.kavindu.testing.domain.entities.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    List<BookEntity> findByAuthor(String titile);
+}
